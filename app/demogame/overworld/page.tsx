6fcc2fd061b28d1a8bd9cc4db192de1f621a286f
adehoog <adehoog@ufl.edu>
2024-11-05 21:17:01 -0500

    added all levels

--- app/demogame/overworld/page.tsx
+++ app/demogame/overworld/page.tsx
@@ -38,10 +38,13 @@ export default function Overworld() {
           //fill={true}
         />
 
-        <div style={{position:"absolute", bottom: 150, right: 1000}}>
+        <div style={{position:"absolute", bottom: 100, right: 700}}>
         <Link href="/demogame">
         <Button className="h-9 px-4 py-2" onClick={handleLevel} disabled={false}>
             Level 1
+        </Button>
+        <Button className="h-9 px-4 py-2" onClick={handleLevel} disabled={true}>
+            Level 2
         </Button>
         </Link>
         <Image
@@ -50,6 +53,125 @@ export default function Overworld() {
             //layout="fill"
             width={100}
             height={100}
+            objectFit="scale-down"
+            className=""
+            />
+        </div>
+
+        <div style={{position:"absolute", bottom: 50, right: 100}}>
+        <Link href="/demogame">
+        <Button className="h-9 px-4 py-2" onClick={handleLevel} disabled={true}>
+            Level 3
+        </Button>
+        </Link>
+        <Image
+            src="/game/level_objects/C_storage lockers.png"
+            alt="terminals"
+            //layout="fill"
+            width={50}
+            height={50}
+            objectFit="scale-down"
+            className=""
+            />
+        </div>
+
+        <div style={{position:"absolute", bottom: 500, right: 500}}>
+        <Link href="/demogame">
+        <Button className="h-9 px-4 py-2" onClick={handleLevel} disabled={true}>
+            Level 4
+        </Button>
+        </Link>
+        <Image
+            src="/game/level_objects/D_temperature control.png"
+            alt="terminals"
+            //layout="fill"
+            width={20}
+            height={20}
+            objectFit="scale-down"
+            className=""
+            />
+        </div>
+
+        <div style={{position:"absolute", bottom: 350, right: 700}}>
+        <Link href="/demogame">
+        <Button className="h-9 px-4 py-2" onClick={handleLevel} disabled={true}>
+            Level 5
+        </Button>
+        </Link>
+        <Image
+            src="/game/level_objects/E_labeling machine.png"
+            alt="terminals"
+            //layout="fill"
+            width={350}
+            height={350}
+            objectFit="scale-down"
+            className=""
+            />
+        </div>
+
+        <div style={{position:"absolute", bottom: 350, right: 200}}>
+        <Link href="/demogame">
+        <Button className="h-9 px-4 py-2" onClick={handleLevel} disabled={true}>
+            Level 6
+        </Button>
+        </Link>
+        <Image
+            src="/game/level_objects/F_alarm system.png"
+            alt="terminals"
+            //layout="fill"
+            width={100}
+            height={100}
+            objectFit="scale-down"
+            className=""
+            />
+        </div>
+
+        <div style={{position:"absolute", bottom: 200, right: 400}}>
+        <Link href="/demogame">
+        <Button className="h-9 px-4 py-2" onClick={handleLevel} disabled={true}>
+            Level 7
+        </Button>
+        </Link>
+        <Image
+            src="/game/level_objects/G_sorting machine.png"
+            alt="terminals"
+            //layout="fill"
+            width={100}
+            height={100}
+            objectFit="scale-down"
+            className=""
+            />
+        </div>
+
+        <div style={{position:"absolute", bottom: 100, right: 1000}}>
+        <Link href="/demogame">
+        <Button className="h-9 px-4 py-2" onClick={handleLevel} disabled={true}>
+            Level 8
+        </Button>
+        </Link>
+        <Image
+            src="/game/level_objects/I_plant monitor.png"
+            alt="terminals"
+            //layout="fill"
+            width={200}
+            height={200}
+            objectFit="scale-down"
+            className=""
+            />
+        </div>
+
+        <div style={{position:"absolute", bottom: 350, right: 1100}}>
+        <Link href="/demogame">
+        <Button className="h-9 px-4 py-2" onClick={handleLevel} disabled={true}>
+            Level 9
+        </Button>
+        </Link>
+        <Image
+            src="/game/level_objects/J_modular device rack.png"
+            alt="terminals"
+            //layout="fill"
+            width={75}
+            height={75}
             objectFit="scale-down"
             className=""
             />
